612817a91aa95b884ee1225547d1ec24dcefcb8e
Yūri Katsuki <yuri@katsuki.moe>
2023-05-03 10:32:53 +0500

    updating contributor social link

--- src/components/Contributor.jsx
+++ src/components/Contributor.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import { GridPattern } from '@/components/GridPattern'
 import { SectionHeading } from '@/components/SectionHeading'
-import authorImage from '@/images/avatars/author.jpg'
+import authorImage from '@/images/avatars/contributor.jpg'
 
 function TwitterIcon(props) {
   return (
@@ -11,6 +11,14 @@ function TwitterIcon(props) {
       <path d="M13.817 33.753c12.579 0 19.459-10.422 19.459-19.458 0-.297 0-.592-.02-.884a13.913 13.913 0 0 0 3.411-3.543 13.65 13.65 0 0 1-3.928 1.077 6.864 6.864 0 0 0 3.007-3.784 13.707 13.707 0 0 1-4.342 1.66 6.845 6.845 0 0 0-11.655 6.239A19.417 19.417 0 0 1 5.654 7.915a6.843 6.843 0 0 0 2.117 9.128 6.786 6.786 0 0 1-3.104-.853v.086a6.842 6.842 0 0 0 5.487 6.704 6.825 6.825 0 0 1-3.088.116 6.847 6.847 0 0 0 6.39 4.75A13.721 13.721 0 0 1 3.334 30.68a19.36 19.36 0 0 0 10.483 3.066" />
     </svg>
   )
+}
+
+function TelegramIcon(props) {
+    return (
+        <svg aria-hidden="true" viewBox="0 0 64 64" {...props}>
+            <path d="M32,10c12.15,0,22,9.85,22,22s-9.85,22-22,22s-22-9.85-22-22S19.85,10,32,10z M39.589,40.968	c0.404-1.241,2.301-13.615,2.534-16.054c0.071-0.738-0.163-1.229-0.619-1.449c-0.553-0.265-1.371-0.133-2.322,0.21	c-1.303,0.47-17.958,7.541-18.92,7.951c-0.912,0.388-1.775,0.81-1.775,1.423c0,0.431,0.256,0.673,0.96,0.924	c0.732,0.261,2.577,0.82,3.668,1.121c1.05,0.29,2.243,0.038,2.913-0.378c0.709-0.441,8.901-5.921,9.488-6.402	c0.587-0.48,1.056,0.135,0.576,0.616c-0.48,0.48-6.102,5.937-6.844,6.693c-0.901,0.917-0.262,1.868,0.343,2.249	c0.689,0.435,5.649,3.761,6.396,4.295c0.747,0.534,1.504,0.776,2.198,0.776C38.879,42.942,39.244,42.028,39.589,40.968z" />
+        </svg>
+    )
 }
 
 export function Contributor() {
@@ -54,11 +62,11 @@ export function Contributor() {
             </p>
             <p className="mt-8">
               <Link
-                href="https://twitter.com/yurionterminal"
+                href="https://t.me/Otabek_Ismoilov"
                 className="inline-flex items-center text-base font-medium tracking-tight text-blue-600"
               >
-                <TwitterIcon className="h-10 w-10 fill-current" />
-                <span className="ml-4">Twitter da obuna bo'lish</span>
+                <TelegramIcon className="h-14 w-14 fill-current" />
+                <span className="ml-4">Telegram da obuna bo'lish</span>
               </Link>
             </p>
           </div>
